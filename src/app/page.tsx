c18d6832bf32f3530a1baa5a7ca98df0f402a47b
rynahmed101-sys <ryn.ahmed101@gmail.com>
2026-09-22 13:48:32 +0500

    Use browser admin gate instead of app session redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import MirrorDashboard from "@/components/MirrorDashboard";
-import { extractCookieToken, verifyAdminSession } from "@/lib/auth";
 
-export default async function Home() {
-  const cookieStore = await cookies();
-  const session = await verifyAdminSession(
-    extractCookieToken(cookieStore.toString())
-  );
-
-  if (!session) {
-    redirect("/admin");
-  }
-
+export default function Home() {
   return <MirrorDashboard />;
 }
